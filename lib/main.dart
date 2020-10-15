6bdacc9e0e1416abbcfd002bfb95d80908e109b0
zubeen <zubeenqadry@gmail.com>
2020-10-15 07:07:31 +0530

    first commit columns and rows challenge

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,59 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    Myapp()
+  );
+}
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                height: double.infinity,
+              ),
+              SizedBox(
+                width: 46.0,
+              ),
+              Container(
+
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 46.0,
+              ),
+
+              Container(
+
+                width: 100.0,
+                color: Colors.blue,
+                height: double.infinity,
+              ),
+            ],
+          ),
+        )
       ),
-    ),
-  );
+    );
+  }
 }
