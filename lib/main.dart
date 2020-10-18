7a33addb1103a8d7e272091eccc1561b3fb46345
zubeen <zubeenqadry@gmail.com>
2020-10-19 03:15:47 +0530

    completed the mi flutter app - Section 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,30 +13,73 @@ class Myapp extends StatelessWidget {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
-             children: [
-               CircleAvatar(
-                 radius: 50,
-                 backgroundImage: AssetImage('images/zubeen.PNG'),
-               ),
-               Text(
-                 'Syed Zubeen',
-                 style: TextStyle(
-                   fontSize: 40.0,
-                   fontWeight: FontWeight.bold,
-                   color: Colors.white,
-                   fontFamily: 'Pacifico',
-                 ),
-               ),
-               Text(
-                 'Software Engineer',
-                 style: TextStyle(
-                   fontSize: 17.0,
-                   letterSpacing: 2.5,
-                   fontWeight: FontWeight.bold,
-                   color: Colors.white,
-                 ),
-               )
-             ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/zubeen.PNG'),
+                ),
+                Text(
+                  'Syed Zubeen',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    fontFamily: 'SourceSansPro-Regular',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade900,
+
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                    ),
+                    title: Text(
+                      '+91 7503765007',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro-Regular',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail_rounded,
+                      ),
+                      title: Text(
+                        'zubeenqadry@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro-Regular',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+              ],
             ),
           )),
     );
